4c91d344da42f183d239fa13a56cfe6cd167c50b
FynnFrei <fynn.freist@gmail.com>
2022-02-09 21:08:11 +0100

    MultiTrack

--- AR_Maskottchen/Assets/Scripts/FoodCollider.cs
+++ AR_Maskottchen/Assets/Scripts/FoodCollider.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Maskottchen.Manager;
 
 public class FoodCollider : MonoBehaviour
 {
+    public Maskottchen_Manager maskottchen_ManagerScript;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.name == "Leos_Mascot")
+        if(collision.collider.CompareTag("Maskottchen"))
         {
             Debug.Log("Collision!");
+            Destroy(gameObject);
+            maskottchen_ManagerScript.Feed();
         }
     }
 }
