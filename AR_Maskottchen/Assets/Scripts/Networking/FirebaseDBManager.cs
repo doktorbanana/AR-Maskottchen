fbf5839415653874e01e812b886c59993170a8a1
lnbs97 <45437638+lnbs97@users.noreply.github.com>
2022-02-10 10:40:07 +0100

    fix firebase on android

--- AR_Maskottchen/Assets/Scripts/Networking/FirebaseDBManager.cs
+++ AR_Maskottchen/Assets/Scripts/Networking/FirebaseDBManager.cs
@@ -8,93 +8,96 @@ using Firebase.Extensions;
 
 using Maskottchen.Manager;
 
-namespace Maskottchen.Database{
-public class FirebaseDBManager : MonoBehaviour
+namespace Maskottchen.Database
 {
-    DatabaseReference reference;
-    Firebase.FirebaseApp app;
-
-    public static GameState gameState = new GameState();
-    void Start()
+    public class FirebaseDBManager : MonoBehaviour
     {
-        reference = FirebaseDatabase.DefaultInstance.RootReference;
-        //FirebaseDatabase.DefaultInstance.GetReference("gamestate").ValueChanged += HandleUpdateGameState;
-    }
+        DatabaseReference reference;
+        Firebase.FirebaseApp app;
 
-/**
-    public void HandleUpdateGameState(object sender, ValueChangedEventArgs args)
-    {
-        DataSnapshot snapshot = args.Snapshot;
-        Debug.Log("Firebase value: " + snapshot.Value);
-        GameState myGameState;
-        myGameState = JsonUtility.FromJson<GameState>(snapshot.GetRawJsonValue());
-        Debug.Log("myObject.food: " + myGameState.food);
-    }
+        public static GameState gameState = new GameState();
+        void Start()
+        {
+            FirebaseCheck();
+            reference = FirebaseDatabase.DefaultInstance.RootReference;
+            Debug.Log(reference);
+            //FirebaseDatabase.DefaultInstance.GetReference("gamestate").ValueChanged += HandleUpdateGameState;
+        }
+
+        /**
+            public void HandleUpdateGameState(object sender, ValueChangedEventArgs args)
+            {
+                DataSnapshot snapshot = args.Snapshot;
+                Debug.Log("Firebase value: " + snapshot.Value);
+                GameState myGameState;
+                myGameState = JsonUtility.FromJson<GameState>(snapshot.GetRawJsonValue());
+                Debug.Log("myObject.food: " + myGameState.food);
+            }
 */
-    public void UpdateGameState(float hungry, float unsatisfied, float tired)
-    {
-        gameState.müde = tired;
-        gameState.zufrieden = unsatisfied;
-        gameState.food = hungry;
-
-        FirebaseDatabase.DefaultInstance.GetReference("gamestate").GetValueAsync().ContinueWith(task =>
+        public void UpdateGameState(float hungry, float unsatisfied, float tired)
         {
-            if (task.IsFaulted)
-            {
-                Debug.LogError(task);
-            }
-            else if (task.IsCompleted)
-            {
-                DataSnapshot snapshot = task.Result;
-                string json = JsonUtility.ToJson(gameState);
-                reference.Child("gamestate").SetRawJsonValueAsync(json);
-            }
-        });
-    }
+            gameState.müde = tired;
+            gameState.zufrieden = unsatisfied;
+            gameState.food = hungry;
 
-    public static void GetGameState()
-    {
-        FirebaseDatabase.DefaultInstance.GetReference("gamestate").GetValueAsync().ContinueWithOnMainThread(task =>
+            FirebaseDatabase.DefaultInstance.GetReference("gamestate").GetValueAsync().ContinueWith(task =>
+            {
+                if (task.IsFaulted)
+                {
+                    Debug.LogError(task);
+                }
+                else if (task.IsCompleted)
+                {
+                    DataSnapshot snapshot = task.Result;
+                    string json = JsonUtility.ToJson(gameState);
+                    reference.Child("gamestate").SetRawJsonValueAsync(json);
+                }
+            });
+        }
+
+        public static void GetGameState()
         {
-            if (task.IsFaulted)
+            FirebaseDatabase.DefaultInstance.GetReference("gamestate").GetValueAsync().ContinueWithOnMainThread(task =>
             {
-                Debug.LogError(task);
-            }
-            else if (task.IsCompleted)
-            {
-                DataSnapshot snapshot = task.Result;
-                gameState = JsonUtility.FromJson<GameState>(snapshot.GetRawJsonValue());
-                Maskottchen_Manager.hungry = gameState.food;
-                Maskottchen_Manager.tired = gameState.müde;
-                Maskottchen_Manager.unsatisfied = gameState.zufrieden;
+                if (task.IsFaulted)
+                {
+                    Debug.LogError(task);
+                }
+                else if (task.IsCompleted)
+                {
+                    DataSnapshot snapshot = task.Result;
+                    gameState = JsonUtility.FromJson<GameState>(snapshot.GetRawJsonValue());
+                    Maskottchen_Manager.hungry = gameState.food;
+                    Maskottchen_Manager.tired = gameState.müde;
+                    Maskottchen_Manager.unsatisfied = gameState.zufrieden;
 
-            }
-        });
-    }
+                }
+            });
+        }
 
-    public bool FirebaseCheck()
-    {
-        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        public bool FirebaseCheck()
         {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
             {
+                var dependencyStatus = task.Result;
+                if (dependencyStatus == Firebase.DependencyStatus.Available)
+                {
                 // Create and hold a reference to your FirebaseApp,
                 // where app is a Firebase.FirebaseApp property of your application class.
                 app = Firebase.FirebaseApp.DefaultInstance;
 
                 // Set a flag here to indicate whether Firebase is ready to use by your app.
                 return true;
-            }
-            else
-            {
-                UnityEngine.Debug.LogError(System.String.Format(
-                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                }
+                else
+                {
+                    UnityEngine.Debug.LogError(System.String.Format(
+                      "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
                 // Firebase Unity SDK is not safe to use here.
                 return false;
-            }
-        });
-        return false;
+                }
+            });
+            return false;
+        }
     }
-}
 }
